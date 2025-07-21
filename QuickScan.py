74e809f9f1ff5d88f36f4960c32a53b68f3b9c79
M1T730 <junbaomatteo.q@students.opit.com>
2025-07-21 22:31:00 +0200

    CHANGELOG of Lorenzo's code to make it more scalable and readable
    CHANGELOG:
    	•	Renamed informations to resources for clarity and consistency.
    	•	Created a new file engine_rules.py, moved all rule lists and the severity_evaluation function into it.
    	•	Modified the main script to output a single dictionary instead of multiple print statements (this simplifies JSON conversion), without changing the individual function outputs( in the process the outputs names were changed to better match the specific rules we are searching)
    	•	Added the append_result function to simplify and standardize result handling in the main logic.
    	•	Clarified that we are checking for Security Groups within EC2 resources, not standalone Security Group types.
    	•	Added general checks for AWS service categories in resource types (e.g., S3, IAM, EC2…).
    	•	Added specific type checks for AWS resource types (e.g., AWS::S3::Bucket, AWS::IAM::Role…).
    	•	Renamed s3_path function and split it into two separate functions:
    	•	s3_check_public_access
    	•	s3_check_encryption
    	•	Renamed securityGroup_path to ec2_check_securityGroups.
    	•	Renamed iam_path to iam_check_role.
    	•	Renamed rds_path to rds_check_dbinstance.
    	•	Renamed coludFormation_template_2.json to cloudFormation_template_2json
    
    Side note: No internal logic of the functions was changed, except for splitting the original S3 function into two distinct parts

--- QuickScan.py
+++ QuickScan.py
@@ -1,143 +1,73 @@
 import json as j
+from rule_engine import (
+    s3_check_public_access,
+    s3_check_encryption,
+    ec2_check_securitygroups,
+    iam_check_role,
+    rds_check_dbinstance,
+)
 
 with open ('cloudFormation_template.json', 'r') as file:
     data = j.load(file)
-  
+
 #list of s3 rules  
-list_of_s3_rules = ['BlockPublicAcls', 'IgnorePublicAcls', 'BlockPublicPolicy', 'RestrictPublicBuckets']
+s3_public_access_rules = ['BlockPublicAcls', 'IgnorePublicAcls', 'BlockPublicPolicy', 'RestrictPublicBuckets']
 #list of SecurityGroup rules
 list_of_securityGroup_rules = ['FromPort', 'ToPort']
 #list for IAM rules
 list_of_iam_rules = ['Action']
 #list of RDS rules
 list_of_rds_rules = ['StorageEncrypted']
-
-#function to evaluate the severity of the rule
-def severity_evaluation(rule):
-    high_severity = {"open", "public", "*", "0.0.0.0", "ssh", "rdp", "internet", "admin", "fromport", "toport"}
-    medium_severity = {"encryption", "unencrypted", "policy", "role", "privilege", "logging", "audit", "kms", "encrypted"}
-    low_severity = {"naming", "tag", "versioning", "backup", "idle", "default", "description"}
-
-    rule_lower = rule.lower()
-    #check for high severity keywords
-    if any(keyword in rule_lower for keyword in high_severity):
-        return "High"
-    #check for medium severity keywords
-    if any(keyword in rule_lower for keyword in medium_severity):
-        return "Medium"
-    #check for low severity keywords
-    if any(keyword in rule_lower for keyword in low_severity):
-        return "Low"
-    
-    
-#fucntion to search for threats in the S3 buckets settings
-def s3_path(list_of_s3_rules, value):
-    #return dict containing the rules that represent a threat
-    dangerous_rules = {'Type' : 'S3'}
-    
-    #loop to check each rule againts the json
-    
-    for i in list_of_s3_rules:
-        try:
-            if (value['Properties']['PublicAccessBlockConfiguration'][i]) == False:
-                dangerous_rules[i] = severity_evaluation(i)
-        except KeyError:
-            continue
-    
-    try:
-        path = value['Properties']['BucketEncryption']['ServerSideEncryptionConfiguration'][0]['ServerSideEncryptionByDefault']['SSEAlgorithm']
-        path_2 = value['Properties']['BucketEncryption']['ServerSideEncryptionConfiguration'][0]['ServerSideEncryptionByDefault']['KMSMasterKeyID']
-    except KeyError:
-        path_2 = False
-        
-    if (path == "AES256"):
-        return dangerous_rules
-    elif (path == 'aws:kms' and path_2):
-        return dangerous_rules        
-    else:
-        dangerous_rules['ServerSideEncryption'] = severity_evaluation('ServerSideEncryptionConfiguration')
-        
-    return dangerous_rules
-
-
-#function to search for for threats in the SecurityGroup settings
-def securityGroup_path(list_of_securityGroup_rules, iter, value):
-    #return dict containing the rules that represent a threat
-    dangerous_rules = {'Type' : 'SecurityGroup'}
-    
-    #loop inside the list that contain the securityGroup properties
-    for i in list_of_securityGroup_rules:
-        try:
-            if (value['Properties']['SecurityGroupIngress'][iter][i] in [22, 3389]):
-                if (value['Properties']['SecurityGroupIngress'][iter]["CidrIp"] == "0.0.0.0/0"):
-                    dangerous_rules[i] = severity_evaluation(i)
-        except KeyError:
-            continue     
-    
-    return dangerous_rules
-
-
-#fuction to search for threats in the IAM settings
-def iam_path(list_of_iam_rules, value):
-    #return dict containing the rules that represent a threat
-    dangerous_rules = {'Type' : 'IAM'}
-    
-    #loop inside the list that contain the IAM properties
-    for i in list_of_iam_rules:
-        try:
-            if (value['Properties']['AssumeRolePolicyDocument']['Statement'][0][i] == '*' or
-                value['Properties']['Policies'][0]['PolicyDocument']['Statement'][0][i] == '*'):
-                
-                dangerous_rules['Actions'] = severity_evaluation('*')
-        except KeyError:
-            continue
-    
-    return dangerous_rules
-
-
-#function to search threats in the RDS settings
-def rds_path(list_of_rds_rules, value):
-    #return dict containing the rules that represent a threat
-    dangerous_rules = {'Type' : 'RDS'}
-    
-    #loop inside the list that contain the RDS properties
-    for i in list_of_rds_rules:
-        if(value['Properties'][i]) == False:
-            dangerous_rules[i] = severity_evaluation(i)
-    
-    return dangerous_rules
-
+def append_result(result, type_name, all_findings):
+    if len(result) > 0:
+        if type_name not in all_findings:
+            all_findings[type_name] = []
+        all_findings[type_name].append(result)
 
 def threat_check():
+    all_findings = {}
     #loop in the uploded json to check for threats
-    informations = data['Resources']
-    for key, value in informations.items():
-        
+    Reseurces = data['Resources']
+    for key, value in Reseurces.items():
+
         #checking for S3 threaths
         if 'S3' in value['Type']:
-            s3_issues = s3_path(list_of_s3_rules, value)
-            print(s3_issues)
+            # checking for AWS:S3:Bucket threaths
+            if 'S3::Bucket' in value['Type']:
+                result1 = s3_check_public_access(s3_public_access_rules, value)
+                result2 = s3_check_encryption(value)
+
+                for result in [result1, result2]:
+                    type_name = 'AWS::S3::Bucket'
+                    append_result(result, type_name, all_findings)
             
-        #checking for SecurityGroup threats
-        if 'SecurityGroup' in value['Type']:
-            val_type = value['Properties']['SecurityGroupIngress']
-            iteration = 0
-            for rule in val_type:
-                securityGroup_issues = securityGroup_path(list_of_securityGroup_rules, iteration, value)
-                print(securityGroup_issues)
-                iteration += 1
+        #checking for EC2 threats
+        if 'EC2' in value['Type']:
+            # checking for AWS::EC2::SecurityGroup threaths
+            if 'EC2::SecurityGroup' in value['Type']:
+                for i, _ in enumerate(value['Properties'].get('SecurityGroupIngress', [])):
+                    result = ec2_check_securitygroups(list_of_securityGroup_rules, i, value)
+                    type_name = 'AWS::EC2::SecurityGroup'
+                    append_result(result, type_name, all_findings)
                 
         #checking for IAM threats
-        if 'IAM::Role' in value['Type']:
-            iam_issues = iam_path(list_of_iam_rules, value)
-            print(iam_issues)
-            
+        if 'IAM' in value['Type']:
+            # checking for AWS::IAM::Role threaths
+            if 'IAM::Role' in value['Type']:
+                result = iam_check_role(list_of_iam_rules, value)
+                type_name = 'AWS::IAM::Role'
+                append_result(result, type_name, all_findings)
+
         #checking for RDS threats
-        if 'RDS::DBInstance' in value['Type']:
-            rds_issues = rds_path(list_of_rds_rules, value)
-            print(rds_issues)
-   
-            
-threat_check()
+        if 'RDS' in value['Type']:
+            # checking for AWS::RDS::DBInstance threaths
+            if 'RDS::DBInstance' in value['Type']:
+                result = rds_check_dbinstance(list_of_rds_rules, value)
+                type_name = 'AWS::RDS::DBInstance'
+                append_result(result, type_name, all_findings)
 
 
+    return all_findings
+
+
+print(j.dumps(threat_check(), indent=2))
